0b66dd5af0ee5cd53294a945580616dae7aca76d
danix1999 <danielvillotaalban@gmail.com>
2019-06-07 19:03:24 -0500

    Initial commit

---
+++ app/src/main/java/com/sena/INTERSAT/IniciarSesion.java
@@ -0,0 +1,13 @@
+package com.sena.INTERSAT;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IniciarSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
